e65d6dff25e1ce408896ca59ab6e01bd82ab0ee6
RElDerwy <rehab.derwy@gmail.com>
2025-11-07 23:56:51 +0200

    Add files via upload

---
+++ Assignment 1_Rehab ElDerwy/Variables & Data Types.py
@@ -0,0 +1,8 @@
+# Define variables for my info
+name = "Rehab ElDerwy"  
+age = 50
+height = 164.5  # in centimeters
+is_student = False
+
+print(f"Hello, my name is {name}. I am {age} years old and {height} centemeter tall.")
+print(f"My student status is: {is_student}.")
